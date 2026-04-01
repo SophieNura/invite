420382ecb9bb97df094d9894265bdd44daf7c07a
SophieNura <SophieNura@users.noreply.github.com>
2026-04-01 18:00:48 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const revealedSections = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.18,
+  }
+);
+
+revealedSections.forEach((section) => observer.observe(section));
